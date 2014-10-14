3eebae305e6fe7cb56da482a365979f6ab0fa1ed
Andy Myers <andy@igoo.co.uk>
2014-10-14 17:07:28 +0100

    Updated bootstrap and readme

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ phalcon-php_vagrant-box_one
 ===========================
 
 A centos-6.5, php-5.4, phalcon-php, percona-sql vagrant box with Vagrant file and bootstrap.ssh
+
+NOTE: The bootstrap file sets the server time to GB local time. Please amend for your own location.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
-# REMEMBER!! to install the vbguest plugin: vagrant plugin install vagrant-vbguest
-# https://github.com/dotless-de/vagrant-vbguest
+# See video: 
 
 echo Running update
 yum update -y
 
-echo Installing LAMP stack stuff ....
+echo Installing software ....
+
+#Packages
 cd ~
+
+# Percona
 wget http://www.percona.com/redir/downloads/percona-release/percona-release-0.0-1.x86_64.rpm
 sudo rpm -Uvh percona-release-0.0-1.x86_64.rpm
 sudo yum -y install Percona-Server-client-55 Percona-Server-server-55
@@ -14,12 +17,13 @@ sudo yum -y install Percona-Server-client-55 Percona-Server-server-55
 # PHP 5.4 for CentOS 6.x
 sudo rpm -Uvh http://mirror.webtatic.com/yum/el6/latest.rpm
 
+# Install Apache
 sudo yum install -y httpd httpd-devel
-#Install php and other things!
+
+# Install php and other things! Visit https://webtatic.com/packages/php54/ for more info
+# For PHP 5.5 visit https://webtatic.com/packages/php55/ for more info.
 sudo yum install -y pcre-devel php54w php54w-mysql php54w-mcrypt php54w-devel gcc make libtool
 
-
-# For PHP 5.5 visit https://webtatic.com/packages/php55/ for more info.
 
 echo changing httpd file structure
 sudo rm -rf /var/www/html
@@ -59,4 +63,5 @@ echo Creating admin account
 mysql -u root < /vagrant/mysql-setup.sql
 # mysql -u root < /vagrant/my-site.sql
 
+# See YouTube Video about this
 rm /etc/udev/rules.d/70-persistent-net.rules
